5842ec3d275c0819e5db1426bf0ac81bb27a7659
Jonathan Brierley <jonnywb@users.noreply.github.com>
2024-07-09 11:10:38 +0100

    ionic project - init commit

---
+++ android/app/src/main/java/ionic/bookevents/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.bookevents;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.bookevents',
+  appName: 'bookevents',
+  webDir: 'dist'
+};
+
+export default config;
